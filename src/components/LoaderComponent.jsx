f5ae7bb75c79ff3991f0a45c99a005316b9d42f4
vinayakhumberi <vinay.humberi@gmail.com>
2018-10-10 22:45:32 +0530

    code splitting

---
+++ src/components/LoaderComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoaderComponent() {
+  return (
+    <div>
+      loading...
+    </div>
+  );
+}
+
+export default LoaderComponent;
--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,38 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import HomeContainer from './containers/HomeContainer';
 import CartContainer from './containers/CartContainer';
 import AccountContainer from './containers/AccountContainer';
+import LoaderComponent from './components/LoaderComponent';
+import Loadable from 'react-loadable';
+
+const Home = Loadable({
+  loader: () =>
+    import ('./containers/HomeContainer'),
+  loading: LoaderComponent
+});
+
+const Cart = Loadable({
+  loader: () =>
+    import ('./containers/CartContainer'),
+  loading: LoaderComponent
+});
+
+const Account = Loadable({
+  loader: () =>
+    import ('./containers/AccountContainer'),
+  loading: LoaderComponent
+});
+
 
 export default () => {
  return (
    <BrowserRouter>
    	<div>
      <Switch>
-       <Route exact path='/' component={HomeContainer}/>
-       <Route path='/home' component={HomeContainer}/>
-       <Route path='/cart' component={CartContainer}/>
-       <Route path='/account' component={AccountContainer}/>
-       <Route path='/exclusive' component={HomeContainer}/>
+       <Route exact path='/' component={Home}/>
+       <Route path='/home' component={Home}/>
+       <Route path='/cart' component={Cart}/>
+       <Route path='/account' component={Account}/>
+       <Route path='/exclusive' component={Home}/>
        </Switch>
   	</div>
    </BrowserRouter>
